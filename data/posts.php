aff4d4108a62757598ce067e17fd9eb0caab9629
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-10-20 15:58:17 -0300

    Ola

--- data/posts.php
+++ data/posts.php
@@ -11,21 +11,21 @@
     [
       'id' => 2,
       'title' => 'Melhores filmes de 2023, segundo IMDB',
-      'description' => 'Aprenda o paradigma de orientação a objetos com Java, uma linguagem em alta no mercado.',
+      'description' => 'Uma crítica bem fundamentada pode abrir novas perspectivas e aprofundar a apreciação de um filme...',
       'tags' => ['Java', 'OOP', 'Programação Avançada'],
       'img' => 'prog-2.jpg'
     ],
     [
       'id' => 3,
       'title' => '1001 filmes para ver antes de morrer',
-      'description' => 'A lógica de programação é o ponto inicial de muitos estudantes de programação, será que é importante?',
+      'description' => 'Explorar a lista de "1001 Filmes para Ver Antes de Morrer" é uma emocionante jornada através da história do cinema...',
       'tags' => ['lógica', 'programação', 'algoritmos'],
       'img' => 'prog-3.jpg',
     ],
     [
       'id' => 4,
       'title' => 'Lançamentos 2024',
-      'description' => 'Neste post você vai aprender tudo que precisa para ser um bom programador de PHP!',
+      'description' => 'Os lançamentos de filmes programados para 2024 estão gerando grande expectativa entre os cinéfilos...',
       'tags' => ['programação', 'PHP', 'lógica de programação'],
       'img' => 'prog-4.jpg'
     ],
--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,4 @@
-<<<<<<<<< Temporary merge branch 1
-=========
 
->>>>>>>>> Temporary merge branch 2
 <footer>
   <p>Hora de Codar &copy; 2020</p>
 </footer>   
