1c8e99e1f9d40c33a2448d78e8f5e818d7aac41c
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-10-27 10:17:53 -0300

    commit

--- data/posts.php
+++ data/posts.php
@@ -13,14 +13,14 @@
       'title' => 'Melhores filmes de 2023, segundo IMDB',
       'description' => 'Uma crítica bem fundamentada pode abrir novas perspectivas e aprofundar a apreciação de um filme...',
       'tags' => ['Filmes', 'Cinema', 'IMDB2023'],
-      'img' => 'prog-2.jpg'
+      'img' => 'prog-3.jpg'
     ],
     [
       'id' => 3,
       'title' => '1001 filmes para ver antes de morrer',
       'description' => 'Explorar a lista de "1001 Filmes para Ver Antes de Morrer" é uma emocionante jornada através da história do cinema...',
       'tags' => ['Cinema', 'Lista de Filmes', 'Recomendações de Filmes'],
-      'img' => 'prog-3.jpg',
+      'img' => 'prog-2.jpg',
     ],
     [
       'id' => 4,
